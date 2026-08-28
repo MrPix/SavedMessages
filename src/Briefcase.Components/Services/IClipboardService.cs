74f3b7617610d4c9b91a133b9eada09b26652a28
Oleksii Shapovalov <oleksiy.shapovalov@gmail.com>
2026-08-28 16:01:14 +0300

    Remove MAUI app

--- src/Briefcase.Components/Services/IClipboardService.cs
+++
@@ -1,7 +0,0 @@
-namespace Briefcase.Components.Services;
-
-public interface IClipboardService
-{
-    Task<string?> GetTextAsync();
-    Task SetTextAsync(string text);
-}
--- src/Briefcase.Components/Services/IJumpListService.cs
+++
@@ -1,7 +0,0 @@
-namespace Briefcase.Components.Services;
-
-public interface IJumpListService
-{
-    Task UpdateRecentMessagesAsync(IEnumerable<(string title, string arguments)> recentItems);
-    Task ClearAsync();
-}
